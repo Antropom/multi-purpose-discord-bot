a8e16d01a0d8c9eb53b03d378341bfd4f2c4ba6f
Antropom <colletlouis@gmail.com>
2020-11-28 23:51:42 +0100

    Version 1 done

--- index.js
+++ index.js
@@ -52,20 +52,54 @@ client.on('message', function (message) {
   }
 
   if (message.content.startsWith(`${prefix}sondage`)) {
+    // Créé un tableau en séparant les arguments entre parenthèses
     const quotedArgs = commandBody.match(/\w+|"[^"]+"/g)
+
+    const emojis = ['🇦', '🇧', '🇨', '🇩', '🇪', '🇫']
+
+    // Vérification de du nombre d'arguments
+    if (quotedArgs.length <= 3) {
+      message.channel.send(
+        'Veuillez poser une question et proposer au moins deux réponses'
+      )
+      return
+    } else if (quotedArgs.length > 8) {
+      message.channel.send('Veuillez saisir au maximum six réponses')
+      return
+    }
+
+    // On enlève la commande
     quotedArgs.shift()
+
+    const questionTitle = quotedArgs.shift()
+
+    // Création de chaque réponse possible en y ajoutant des émojis
+    const answersWithEmojis = quotedArgs.map(
+      (answer, i) => `${emojis[i]} ${answer}`
+    )
+
+    // Création de la string avec retours à la ligne pour l'embes
+    const stringAnswers = answersWithEmojis.join('\n')
 
     const pollEmbed = new Discord.MessageEmbed()
       .setColor('#0099ff')
-      .setTitle('Sondage')
-      .setAuthor(message.author.username)
+      .setTitle(`Sondage de ${message.author.username}`)
       .addFields({
-        name: quotedArgs[0],
-        value: `${quotedArgs[1]}\n${quotedArgs[2]}`,
+        name: questionTitle.replace(/['"]+/g, ''),
+        value: `${stringAnswers.replace(/['"]+/g, '')}`,
       })
       .setTimestamp()
 
-    message.channel.send(pollEmbed)
+    message.channel
+      .send(pollEmbed)
+      .then((embedMessage) => {
+        answersWithEmojis.forEach((reaction, i) =>
+          embedMessage.react(emojis[i])
+        )
+      })
+      .then(() => {
+        message.delete()
+      })
   }
 })
 
