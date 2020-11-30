7ba3dd9aafeaf572b497ad40ade4d4a92cfba365
Louis Collet <colletlouis@gmail.com>
2020-11-30 14:37:02 +0100

    Exported poll logic to poll.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js')
 const { rollotron } = require('./functions/rollotron')
+const { poll } = require('./functions/poll')
 const { PollDatabase } = require('./functions/poll-database')
 const { foissSlurs } = require('./functions/foissSlurs')
 const config = require('./config.json')
@@ -41,65 +42,7 @@ client.on('message', function (message) {
   }
 
   if (message.content.startsWith(`${prefix}sondage`)) {
-    // Créé un tableau en séparant les arguments entre parenthèses
-    const quotedArgs = commandBody.match(/\w+|"[^"]+"/g)
-
-    const emojis = ['🇦', '🇧', '🇨', '🇩', '🇪', '🇫']
-
-    // Vérification de du nombre d'arguments
-    if (quotedArgs.length <= 3) {
-      message.channel.send(
-        'Veuillez poser une question et proposer au moins deux réponses'
-      )
-      return
-    } else if (quotedArgs.length > 8) {
-      message.channel.send('Veuillez saisir au maximum six réponses')
-      return
-    }
-
-    // On enlève la commande
-    quotedArgs.shift()
-
-    const questionTitle = quotedArgs.shift().replace(/['"]+/g, '')
-
-    // Création de chaque réponse possible en y ajoutant des émojis
-    const answersWithEmojis = quotedArgs.map(
-      (answer, i) => `${emojis[i]} ${answer.replace(/['"]+/g, '')}`
-    )
-
-    database
-      .create(message, questionTitle, answersWithEmojis)
-      .then((pollId) => {
-        if (pollId === -1) {
-          message.channel.send(
-            'Un problème est survenu lors de la création du sondage.'
-          )
-          return
-        }
-        // Création de la string avec retours à la ligne pour l'embes
-        const stringAnswers = answersWithEmojis.join('\n')
-
-        const pollEmbed = new Discord.MessageEmbed()
-          .setColor('#0099ff')
-          .setTitle(`Sondage #${pollId} de ${message.author.username}`)
-          .addFields({
-            name: questionTitle,
-            value: `${stringAnswers}`,
-          })
-          .setTimestamp()
-
-        message.channel
-          .send(pollEmbed)
-          .then((embedMessage) => {
-            answersWithEmojis.forEach((reaction, i) =>
-              embedMessage.react(emojis[i])
-            )
-            database.setMessageId(pollId, embedMessage.id)
-          })
-          .then(() => {
-            message.delete()
-          })
-      })
+    poll(message, commandBody, database, Discord)
   }
 })
 
