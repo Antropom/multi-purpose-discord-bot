dfcbd1f15767fb9bd1145fcda73d983bbbc512fd
Louis Collet <colletlouis@gmail.com>
2020-11-30 14:40:57 +0100

    Removed 'Ping' function

--- index.js
+++ index.js
@@ -27,11 +27,6 @@ client.on('message', function (message) {
   const args = commandBody.split(' ')
   const command = args.shift().toLowerCase()
 
-  if (message.content === `${prefix}ping`) {
-    const timeTaken = Date.now() - message.createdTimestamp
-    message.reply(`Pong! This message had a latency of ${timeTaken}ms.`)
-  }
-
   if (message.content.startsWith(`${prefix}roll`)) {
     rollotron(message, args)
   }
