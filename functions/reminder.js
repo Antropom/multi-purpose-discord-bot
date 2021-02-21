c074dabd6797605563c065ed240cf89be0879807
Louis <colletlouis@gmail.com>
2021-02-21 18:34:01 +0100

    Auto format date

--- functions/reminder.js
+++ functions/reminder.js
@@ -2,9 +2,18 @@ exports.reminder = async (message, commandBody, database, Discord) => {
   const args = commandBody.split(',')
   const mention =
     message.mentions.users.first() || message.mentions.roles.first()
-  const date = args[1]
+  const date = args[1].trim().split(' ')
+  const dateArray = []
+  dateArray.push(date[0].split('/').reverse().join('-'))
+  dateArray.push(date[1].split('h').join(':'))
+  const formattedDate = dateArray.join(' ')
   const text = args[2]
 
-  const eventId = await database.create(message, mention.id, date, text)
+  const eventId = await database.create(
+    message,
+    mention.id,
+    formattedDate,
+    text
+  )
   message.channel.send(`Rappel créé avec l'id : ${eventId}`)
 }
